b9a6786ed32008717624b5fa1eec9dc4c2c9318a
ankurparasharpandey <ankurparasharpandey@gmail.com>
2016-08-29 02:08:05 +0530

    Update findrepeatednum.java

--- findrepeatednum.java
+++ findrepeatednum.java
@@ -1,13 +1,19 @@
 import java.util.Arrays;
 class repeatedno{
     public static void main(String[] args) {
-    int[] num = { 1, 5, 23, 2, 1, 6, 3, 1, 8, 12, 3,0,0};
+    int flag=0;
+    int[] num = { 1, 2,0};
     Arrays.sort(num);
 
     for(int i = 1; i < num.length; i++) {
     if(num[i] == num[i - 1]) {
         System.out.println("numbers repeated " + num[i]);
+        
     }
+    else flag=1;
+    }
+    if(flag==1) System.out.println("no number is repeated");
 }
 }
-}
+
+     
